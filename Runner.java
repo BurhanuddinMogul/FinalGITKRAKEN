3bbb83688760cc63e27337cbe85548930d8ba5a1
Burhanuddin <bmtech5253@gmail.com>
2019-06-11 09:45:41 -0400

    Single Runner

--- Runner.java
+++ Runner.java
@@ -2,6 +2,17 @@ public class Runner{
 
     public static void main(String[]args){
 
+        Perfect_Square pS = (int x)-> {
+			double root = Math.sqrt(x);
+			return (root - Math.floor(root) == 0);
+		};
+
+        Partner2 p = (float f1, float f2)-> {
+			float q = f1/f2;
+			System.out.printf("%.3f", q);
+		};
+		p.quotient(3,4);
+
     }
 
 }
